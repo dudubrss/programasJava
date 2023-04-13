b2b9c60675f3eb4ec797deec02be90fbb1aef272
Eduardo Barros Silva de Sá <117393799+dudubrss@users.noreply.github.com>
2023-04-13 16:15:20 -0300

    Update Pessoa.java

--- Pessoa.java
+++ Pessoa.java
@@ -7,8 +7,10 @@ public class Pessoa {
     private String nome;
     private Double altura;
     private Double peso;
+    //declaração de atributos da classe pessoa
     
     public Pessoa(String nome, Double altura, Double peso){
+        //contrutor da classe pessoa com parâmetros
         this.nome = nome;
         this.altura = altura;
         this.peso = peso;
@@ -34,7 +36,10 @@ public class Pessoa {
     public void setPeso(Double peso) {
         this.peso = peso;
     }
+    //métodos get/set para retornar e definir variáveis
+    
  public void printPessoa(){
+     //método para imprimir no console os atributos declarados
      System.out.println(" nome :" + nome + "\n altura :" + altura + "\n peso :" + peso);
  }
  
