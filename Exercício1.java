0e5461de69f9e3d7cf13d1361388a9ea80513d48
Eduardo Barros Silva de Sá <117393799+dudubrss@users.noreply.github.com>
2023-04-13 16:20:05 -0300

    Update Exercício1.java

--- Exercício1.java
+++ Exercício1.java
@@ -7,13 +7,15 @@ public class Exercício1 {
 
     public static void main(String[] args) {
        Pessoa p = new Pessoa("zé" , 1.75 , 80.0);
+        //contrutor manual
        Funcionario X2 = new Funcionario("beto" , 1.65 , 75.0 , 1000);
+        //contrutor manual
        p.printPessoa();
        //objeto p chama o método printPessoa para imprimir os atributos no console
        X2.printPessoa();
        //objeto X2 chama o método
        X2.printFuncionario();
-       //objeto X2 chama o método printFuncionário para imprimir a matrícula
+       //objeto X2 chama o método printFuncionário para imprimir os atributos da classe Pessoa + atributo matrícula
     }
     
 }
