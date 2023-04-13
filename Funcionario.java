bceae496867d643ae4a79f27e45810ab78554852
Eduardo Barros Silva de Sá <117393799+dudubrss@users.noreply.github.com>
2023-04-13 16:09:00 -0300

    Update Funcionario.java

--- Funcionario.java
+++ Funcionario.java
@@ -1,14 +1,14 @@
 
 package exercício1;
 
-
+//classe filha herdeira de Pessoa
 public class Funcionario extends Pessoa{ 
-//extends + nome da classe herda os atributos da classe pessoa
+//extends + nome da classe herda os atributos String Double e Double da classe pessoa
     public int matricula;
     
     public Funcionario(String nome,Double altura,Double peso, int matricula){
         super(nome, altura, peso);
-        //super herda todos os métodos e atributos e são atribuídos aos parâmetros dentro do construtor funcionário
+        //super herda todos os métodos e atributos e são atribuídos aos parâmetros dentro do construtor Pessoa
         this.matricula = matricula;
     }
         public void printFuncionario(){
